d89a7688f6c0edbde13b4d5ab5873db94d0ea11b
Frank-G-M <frankmayorgaguerrero@gmail.com>
2025-03-04 19:38:19 -0500

    CRUD users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,49 @@
 class Api::UsersController < ApplicationController
+before_action :set_user, only: [:show, :update, :destroy]
+
+
+  def create 
+  name = params[:name]
+  email = params[:email]
+  password_digest = params[:password_digest]
+  user = User.new(name: name, email: email, password_digest: password_digest)
+  if user.save
+    render json: user
+  else
+    render json: {errors: user.errors.full_messages}, status: 422
+  end
+end
+
+def show
+ render json: @user
+end
+
+
+def update
+  if @user.update(user_params)
+    render json: @user
+  else
+    render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  @user.destroy
+  render json: {message: "User deleted"}
+end
+
+private
+
+
+def set_user
+@user = User.find_by(id: params[:id])
+render json: {error: "User not found"}, status: 404 unless @user
+end
+
+def user_params
+params.require(:user).permit(:name, :email, :password_digest, :role)
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # CRUD API for users
+  post "api/users/create" => "api/users#create"
+  get "api/users/:id" => "api/users#show"
+  match "api/users/:id" => "api/users#update", via: [:patch, :put]
+  delete "api/users/:id" => "api/users#destroy"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
